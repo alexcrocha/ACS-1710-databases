2694af05fe709f459547b266cea16b47ec8b958b
Alex Rocha <rocha.amc@gmail.com>
2022-12-06 21:10:42 -0800

    Complete create, plant, harvest, and edit routes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template, url_for
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 ############################################################
@@ -7,9 +8,12 @@ from bson.objectid import ObjectId
 ############################################################
 
 app = Flask(__name__)
-
-app.config["MONGO_URI"] = "mongodb://localhost:27017/plantsDatabase"
-mongo = PyMongo(app)
+client = MongoClient('localhost', 27017)
+db = client.plants_db
+plants = db.plants
+harvests = db.harvests
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/plantsDatabase"
+# mongo = PyMongo(app)
 
 ############################################################
 # ROUTES
@@ -37,19 +41,22 @@ def about():
 def create():
     """Display the plant creation page & process data from the creation form."""
     if request.method == 'POST':
-        # TODO: Get the new plant's name, variety, photo, & date planted, and 
+        # Get the new plant's name, variety, photo, & date planted, and
         # store them in the object below.
-        new_plant = {
-            'name': '',
-            'variety': '',
-            'photo_url': '',
-            'date_planted': ''
-        }
-        # TODO: Make an `insert_one` database call to insert the object into the
-        # database's `plants` collection, and get its inserted id. Pass the 
-        # inserted id into the redirect call below.
 
-        return redirect(url_for('detail', plant_id=''))
+        new_plant = {
+            'name': request.form.get('plant_name'),
+            'variety': request.form.get('variety'),
+            'photo': request.form.get('photo'),
+            'date_planted': request.form.get('date_planted'),
+        }
+        # Make an `insert_one` database call to insert the object into the
+        # database's `plants` collection, and get its inserted id. Pass the
+        # inserted id into the redirect call below.
+        plants.insert_one(new_plant)
+        inserted_id = plants.find_one(new_plant).get('_id')
+
+        return redirect(url_for('detail', plant_id=inserted_id))
 
     else:
         return render_template('create.html')
@@ -58,19 +65,19 @@ def create():
 def detail(plant_id):
     """Display the plant detail page & process data from the harvest form."""
 
-    # TODO: Replace the following line with a database call to retrieve *one*
+    # Replace the following line with a database call to retrieve *one*
     # plant from the database, whose id matches the id passed in via the URL.
-    plant_to_show = ''
+    plant_to_show = plants.find_one(ObjectId(plant_id))
 
-    # TODO: Use the `find` database operation to find all harvests for the
+    # Use the `find` database operation to find all harvests for the
     # plant's id.
     # HINT: This query should be on the `harvests` collection, not the `plants`
     # collection.
-    harvests = ''
+    all_harvests = harvests.find({'plant_id': plant_id})
 
     context = {
         'plant' : plant_to_show,
-        'harvests': harvests
+        'harvests': all_harvests
     }
     return render_template('detail.html', **context)
 
@@ -80,16 +87,18 @@ def harvest(plant_id):
     Accepts a POST request with data for 1 harvest and inserts into database.
     """
 
-    # TODO: Create a new harvest object by passing in the form data from the
+    # Create a new harvest object by passing in the form data from the
     # detail page form.
+
     new_harvest = {
-        'quantity': '', # e.g. '3 tomatoes'
-        'date': '',
+        'quantity': request.form.get('harvested_amount'), # e.g. '3 tomatoes'
+        'date': request.form.get('date_planted'),
         'plant_id': plant_id
     }
 
-    # TODO: Make an `insert_one` database call to insert the object into the 
+    # Make an `insert_one` database call to insert the object into the
     # `harvests` collection of the database.
+    harvests.insert_one(new_harvest)
 
     return redirect(url_for('detail', plant_id=plant_id))
 
@@ -97,15 +106,21 @@ def harvest(plant_id):
 def edit(plant_id):
     """Shows the edit page and accepts a POST request with edited data."""
     if request.method == 'POST':
-        # TODO: Make an `update_one` database call to update the plant with the
+        # Make an `update_one` database call to update the plant with the
         # given id. Make sure to put the updated fields in the `$set` object.
 
-        
+        plants.update_one({'_id': ObjectId(plant_id)}, {'$set': {
+            'name': request.form.get('plant_name'),
+            'variety': request.form.get('variety'),
+            'photo': request.form.get('photo'),
+            'date_planted': request.form.get('date_planted'),
+        }})
+
         return redirect(url_for('detail', plant_id=plant_id))
     else:
-        # TODO: Make a `find_one` database call to get the plant object with the
+        # Make a `find_one` database call to get the plant object with the
         # passed-in _id.
-        plant_to_show = ''
+        plant_to_show = plants.find_one(ObjectId(plant_id))
 
         context = {
             'plant': plant_to_show
@@ -124,5 +139,5 @@ def delete(plant_id):
     return redirect(url_for('plants_list'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
 
